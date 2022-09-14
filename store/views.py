3c3c71a39c381c4e530c0faa7781a69cadc6fce2
danydurand <danydurand@gmail.com>
2022-09-14 13:00:07 -0400

    Breadcrumbs and Products view
    - The products view was created.
    - The collections view now links to products views
    - Both collections and product views have breadcrumbs to navigate.

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Category
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Category, Product
 
 def home(request):
     return render(request, 'store/index.html')
@@ -12,3 +13,16 @@ def collections(request):
     }
     return render(request, 'store/collections.html', context)
 
+
+def collectionsview(request, slug):
+    if Category.visibles.filter(slug=slug):
+        products = Product.objects.filter(category__slug=slug)
+        category = Category.visibles.filter(slug=slug).first()
+        context = {
+            'products': products,
+            'category': category,
+        }
+        return render(request, 'store/products/index.html', context)
+    else:
+        messages.warning(request, 'No such Category !!')
+        return redirect('collections')
